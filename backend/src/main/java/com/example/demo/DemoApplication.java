0217c3727a9a047cac548a60b2e70ffd7e390b32
V-a-O-s <asteorit01@googlemail.com>
2024-02-21 14:12:07 +0100

    ToDo's, #2

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -26,7 +26,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * The data is converted to a task object using Jackson and added to the list of
  * tasks.
  * All endpoints are annotated with @CrossOrigin to enable cross-origin
- * requests.
+ * requests.-
  * test
  * @author luh
  */
